51bc9ccd6676b1b26e7458877f24a078c137cc98
moohabir <87937848+moohabir@users.noreply.github.com>
2022-06-02 23:54:12 +0200

    wsd

--- src/Tour.js
+++ src/Tour.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Tour({
   id,
   image,
@@ -6,15 +8,21 @@ export default function Tour({
   category,
   description
 }) {
+  const [show, setShow] = useState(false);
   return (
-    <div>
+    <div className="container">
       <header>
         <h4>{category}</h4>
         <img src={image} alt="" className="image" />
       </header>
       <div>
         <h5>{title}</h5>
-        <p>{description}</p>
+        <p>
+          {show ? description : `${description.substring(0, 50)}...`}
+          <button onClick={() => setShow(!show)}>
+            {show ? "show less" : "show more"}
+          </button>
+        </p>
         <p>${price}</p>
       </div>
     </div>
